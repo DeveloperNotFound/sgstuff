74f8dc81d592d351e6065580184c7902d102798d
DeveloperNotFound <72045460+DeveloperNotFound@users.noreply.github.com>
2020-10-17 14:08:37 +0100

    Add files via upload

---
+++ modules/ci_library.js
@@ -0,0 +1,67 @@
+"use strict";
+(function(CyTube_ChatIconsLibrary) {
+    return CyTube_ChatIconsLibrary(
+        window["ChatIcons"])
+})(function(ChatIcons, undefined) {
+    ChatIcons.avatars = Object
+        .assign(ChatIcons.avatars, {
+                        Queen: "https://cdn.discordapp.com/attachments/701588063616499792/736280529388699678/Queen.gif",
+			Changeling01: "https://derpicdn.net/img/view/2012/10/2/111823.png",
+			Changeling02: "https://derpicdn.net/img/view/2012/10/2/111819.png",
+			Changeling03: "https://derpicdn.net/img/view/2012/10/2/111820.png",
+			Changeling04: "https://derpicdn.net/img/view/2012/10/2/111824.png",
+			Changeling05: "https://derpicdn.net/img/view/2012/11/8/145686.png",
+			Changeling06: "https://derpicdn.net/img/view/2012/10/2/111821.png",
+			Starlight01: "https://cdn.discordapp.com/attachments/701588063616499792/702692873572712588/sgequallity.jpg",
+			Aryanne01: "https://derpicdn.net/img/view/2014/4/16/601936.jpg",
+            AuroraChu: "https://cdn.discordapp.com/attachments/701588063616499792/702187177076981811/AuroraChu.png",
+			StarlightBot: "https://cdn.discordapp.com/attachments/701588063616499792/728289348956717196/StarlightBot.jpg",
+			Freg: "https://cdn.discordapp.com/attachments/701588063616499792/728274031735734302/freg.jpg",
+            OneTrueDerp: "https://cdn.discordapp.com/attachments/701588063616499792/712833620590461018/OTD.png",
+            GreatDestinyMan: "https://cdn.discordapp.com/attachments/293112019371163658/701589290706272286/20200419_202456.jpg",
+            PinkMawile: "https://static1.e621.net/data/80/8f/808f9f8fda4787977956d6293e688c40.png",
+            Luzion: "https://derpicdn.net/img/view/2019/9/14/2143560.gif",
+            Cheeselegsfag: "https://derpicdn.net/img/view/2016/9/12/1247357.png",
+			GenH: "https://cdn.discordapp.com/attachments/701588063616499792/728288005915279441/genh.jpeg",
+            Firebolt: "https://cdn.discordapp.com/attachments/701588063616499792/728268598312697936/Firebolt.png",
+			Patrician_Cinephile: "https://cdn.discordapp.com/attachments/701588063616499792/706330091202019388/Patrician.png",
+			Boona: "https://derpicdn.net/img/view/2016/2/16/1089756.png",
+			Gunbunny: "https://cdn.discordapp.com/attachments/701588063616499792/712795333704548464/Gunbunny.png",
+            Tropercs: "https://derpicdn.net/img/view/2019/9/3/2135532.png",
+            MoonShard: "https://derpicdn.net/img/view/2015/5/4/888698.png",
+            Kekarot: "https://derpicdn.net/img/2020/7/10/2395509/large.png",
+			Skipsy: "https://derpicdn.net/img/view/2018/9/9/1828818.png",
+			TollesBrot: "https://derpicdn.net/img/view/2019/6/24/2073875.png",
+			Sparkelyfox: "https://cdn.discordapp.com/attachments/320159328718028800/724270041591185619/HitoshiShinso.jpg",
+            Spyromancy: "https://cdn.discordapp.com/attachments/701588063616499792/724195885143293992/Spyromancy.png"
+        });
+    Object.assign(ChatIcons.users, {
+        aurorachu: "Queen",
+        aurorabot: "AuroraChu",
+        starlightbot: "StarlightBot",
+        aryannebot: "Aryanne01",
+        freg: "Freg",
+        onetruederp: "OneTrueDerp",
+        greatdestinyman: "GreatDestinyMan",
+        cheeselegsfag: "Cheeselegsfag",
+		pinkmawile: "PinkMawile",
+		remark0: "Changeling02",
+		luzion: "Luzion",
+		genh: "GenH",
+        firebolt1089: "Firebolt",
+		reasor: "Changeling04",
+		tropercs: "Tropercs",
+		skipsy: "Skipsy",
+		patrician_cinephile: "Patrician_Cinephile",
+		letni: "Changeling01",
+		boona: "Boona",
+		gunbunny: "Gunbunny",
+		moonshard: "MoonShard",
+        kekarot: "Kekarot",
+        spyromancy: "Spyromancy",
+        tollesbrot: "TollesBrot",
+        sparkelyfox: "Sparkelyfox",
+        ruralgaming: "Changeling05"
+    });
+    ChatIcons.update()
+});
