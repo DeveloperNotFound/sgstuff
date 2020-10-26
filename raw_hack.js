d8de5e81b82642bbc9b9b78e400498aaa69603fd
DarkAuroraChu <65683653+DarkAuroraChu@users.noreply.github.com>
2020-10-26 19:57:12 +0100

    Update raw_hack.js

--- raw_hack.js
+++ raw_hack.js
@@ -61,110 +61,110 @@ if (!this[CHANNEL.name].favicon) {
         settings: {
             active: 1,
             rank: -1,
-            url: "https://cdn.jsdelivr.net/gh/DeveloperNotFound/sgstuff/modules/settings.js",
+            url: "https://raw.githack.com/DeveloperNotFound/sgstuff/modules/settings.js",
             done: true
         },
         privmsg: {
             active: 1,
             rank: 1,
-            url: "https://cdn.jsdelivr.net/gh/DeveloperNotFound/sgstuff/modules/privmsg.js",
+            url: "https://raw.githack.com/DeveloperNotFound/sgstuff/modules/privmsg.js",
             done: true
         },
         whispers: {
             active: 1,
             rank: -1,
-            url: "https://cdn.jsdelivr.net/gh/DeveloperNotFound/sgstuff/modules/whispers.js",
+            url: "https://raw.githack.com/DeveloperNotFound/sgstuff/modules/whispers.js",
             done: true
         },
         userlist: {
             active: 1,
             rank: -1,
-            url: "https://cdn.jsdelivr.net/gh/DeveloperNotFound/sgstuff/modules/userlist.js",
+            url: "https://raw.githack.com/DeveloperNotFound/sgstuff/modules/userlist.js",
             done: true
         },
         md5hash: {
             active: 1,
             rank: -1,
-            url: "https://cdn.jsdelivr.net/gh/DeveloperNotFound/sgstuff/modules/md5hash.js",
+            url: "https://raw.githack.com/DeveloperNotFound/sgstuff/modules/md5hash.js",
             done: true
         },
         designator: {
             active: 1,
             rank: -1,
-            url: "https://cdn.jsdelivr.net/gh/DeveloperNotFound/sgstuff/modules/designator.js",
+            url: "https://raw.githack.com/DeveloperNotFound/sgstuff/modules/designator.js",
             done: true
         },
         playlist: {
             active: 1,
             rank: -1,
-            url: "https://cdn.jsdelivr.net/gh/DeveloperNotFound/sgstuff/modules/playlist.js",
+            url: "https://raw.githack.com/DeveloperNotFound/sgstuff/modules/playlist.js",
             done: true
         },
         notifier: {
             active: 1,
             rank: -1,
-            url: "https://cdn.jsdelivr.net/gh/DeveloperNotFound/sgstuff/modules/playlist.js",
+            url: "https://raw.githack.com/DeveloperNotFound/sgstuff/modules/playlist.js",
             done: true
         },
         chatline: {
             active: 1,
             rank: -1,
-            url: "https://cdn.jsdelivr.net/gh/DeveloperNotFound/sgstuff/modules/chatline.js",
+            url: "https://raw.githack.com/DeveloperNotFound/sgstuff/modules/chatline.js",
             done: true
         },
         chatext: {
             active: 1,
             rank: -1,
-            url: "https://cdn.jsdelivr.net/gh/DeveloperNotFound/sgstuff/modules/chattext.js",
+            url: "https://raw.githack.com/DeveloperNotFound/sgstuff/modules/chattext.js",
             done: true
         },
         chatcolor: {
             active: 1,
             rank: -1,
-            url: "https://cdn.jsdelivr.net/gh/DeveloperNotFound/sgstuff/modules/chattext.js",
+            url: "https://raw.githack.com/DeveloperNotFound/sgstuff/modules/chattext.js",
             done: true
         },
         colormap: {
             active: 1,
             rank: -1,
-            url: "https://cdn.jsdelivr.net/gh/DeveloperNotFound/sgstuff/modules/colormap.js",
+            url: "https://raw.githack.com/DeveloperNotFound/sgstuff/modules/colormap.js",
             done: true
         },
         layout: {
             active: 1,
             rank: -1,
-            url: "https://cdn.jsdelivr.net/gh/DeveloperNotFound/sgstuff/modules/layout.js",
+            url: "https://raw.githack.com/DeveloperNotFound/sgstuff/modules/layout.js",
             done: true
         },
         various: {
             active: 1,
             rank: -1,
-            url: "https://cdn.jsdelivr.net/gh/DeveloperNotFound/sgstuff/modules/various.js",
+            url: "https://raw.githack.com/DeveloperNotFound/sgstuff/modules/various.js",
             done: true
         },
         embedmedia: {
             active: 1,
             rank: -1,
-            url: "https://cdn.jsdelivr.net/gh/DeveloperNotFound/sgstuff/modules/embedmedia.js",
+            url: "https://raw.githack.com/DeveloperNotFound/sgstuff/modules/embedmedia.js",
             done: true
         },
         chaticons: {
             active: 1,
             rank: -1,
-            url: "https://cdn.jsdelivr.net/gh/DeveloperNotFound/sgstuff/modules/embedmedia.js",
+            url: "https://raw.githack.com/DeveloperNotFound/sgstuff/modules/embedmedia.js",
             done: true
         },
         ci_library: {
             active: 1,
             rank: -1,
-            url: "https://rawcdn.githack.com/DeveloperNotFound/sgstuff/b9a45864e9541e49aecbeaf43f362f3d827e6903/modules/ci_library.js",
+            url: "https://raw.githack.com/DeveloperNotFound/sgstuff/master/modules/ci_library.js",
             done: true,
             cache: false
         },
         time: {
             active: 1,
             rank: -1,
-            url: "https://cdn.jsdelivr.net/gh/DeveloperNotFound/sgstuff/modules/time.js",
+            url: "https://raw.githack.com/DeveloperNotFound/sgstuff/modules/time.js",
             done: true
         }
     },
